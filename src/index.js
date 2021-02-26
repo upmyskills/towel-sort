275b2de7b69b05e49e2beefe97d4bcf614475ece
Koti4AtHome <DevAsus>
2021-02-26 21:01:31 +0300

    feat: create a logic

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  const matris = matrix || [];
+  let allinone = [];
+
+  for (let i = 0; i < matris.length; i++) {
+    if (i%2 !== 0) {
+      for (let k = matris[i].length - 1; k >= 0; k--) {
+        allinone.push(matris[i][k]);
+      }
+    } else {
+      allinone = [...allinone, ...matris[i]];
+    }
+  }
+  return allinone;
 }
